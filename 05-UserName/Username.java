aab4e252ea26e7f3dbe7791d6ee15210d24bf8c2
Qijin Huang <qijinh@nycstudents.net>
2021-10-21 08:36:14 -0400

    10/21/21

--- 05-UserName/Username.java
+++ 05-UserName/Username.java
@@ -5,19 +5,16 @@ public class Username{
       Scanner s = new Scanner(System.in);
 
       //ask the user their first Username
-        System.out.println("Please enter your firstname:");
+        System.out.print("Please enter your firstname:");
         String firstName = s.nextLine();
 
       //ask the user their last Username
-        System.out.println("Please enter your lastname:");
+        System.out.print("Please enter your lastname:");
         String lastName = s.nextLine();
 
       //ask the user their birthday in mm/dd/yy
-        System.out.println("Please enter your DOB in mm/dd/yy:");
+        System.out.print("Hello " + firstName + ", please enter your DOB in mm/dd/yy:");
         String birthday = s.nextLine();
-
-      //generate a username for that person based on their data
-        System.out.println(firstName + " " + lastName + " " + birthday);
 
       //create Username
         String userName = "";
@@ -28,6 +25,21 @@ public class Username{
         //last two letter of lastname
         userName += lastName.substring(lastName.length() - 2);
 
+        //grab the month of birth (must be 2 characters)
+          if (birthday.indexOf("/") == 2){
+            userName += birthday.substring(0, 2);
+          } else {
+            userName += "0";
+            userName += birthday.substring(0, 1);
+          }
+
+        //grab the last 2 digits of the year
+          userName += birthday.substring(substring.length()-2);
+
+        //add domain name
+          userName += "@anonymous.org";
+
+        //username created
         System.out.println("Here's your username, write it down because there's no way getting it again.");
         System.out.println(userName);
 
