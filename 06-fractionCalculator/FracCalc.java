de5dc1c5a9b8a7774a71bf16d3fd3687d8c934d6
Qijin Huang <qijinh@nycstudents.net>
2021-11-16 12:15:47 -0500

    11/16/21

--- 06-fractionCalculator/FracCalc.java
+++ 06-fractionCalculator/FracCalc.java
@@ -1,14 +1,8 @@
 import java.util.*;
 
-public class fracCalc {
-    /**
-     * Prompts user for input, passes that input to produceAnswer, then outputs the result.
-     * @param args - unused
-     */
+public class FracCalc {
+
     public static void main(String[] args){
-        // TODO: Read the input from the user and call produceAnswer with an equation
-        // Checkpoint 1: Create a Scanner, read one line of input, pass that input to produceAnswer, print the result.
-        // Checkpoint 2: Accept user input multiple times.
 
         Scanner s = new Scanner(System.in);
         System.out.print("Please enter input equation: ");
@@ -19,17 +13,14 @@ public class fracCalc {
         String term2 = input.substring(spacePos + 3, input.length());
         String sign = input.substring(spacePos + 1 , spacePos + 2); //checkpoint1
 
-        System.out.println(term1);
-        System.out.println(term2);
-
         term1 = input1(term1);
         term2 = input2(term2);
-        System.out.println(term1);
-        System.out.println(term2);
 
         String result = produceAnswer(sign, term1, term2);
+        result = simpification(result);
         System.out.println(result);
       }
+
 
       public static String input1(String term){
         if (term.contains("/") == true && term.contains("_") == true){
@@ -52,6 +43,7 @@ public class fracCalc {
         }
         return "";
       }
+
 
       public static String input2(String term){
         if (term.contains("/") == true && term.contains("_") == true){
@@ -136,6 +128,7 @@ public class fracCalc {
       return "";
     }
 
+
     public static String addition(int x1, int y1, int x2, int y2){
       if (y1 != y2){
         int numerator = x1 * y2 + x2 * y1;
@@ -164,8 +157,8 @@ public class fracCalc {
         int denominator = y1;
         String result = String.valueOf(numerator + "/" + denominator);
         return result;
+      }
     }
-  }
 
 
     public static String multiplication(int x1, int y1, int x2, int y2){
@@ -183,32 +176,15 @@ public class fracCalc {
     }
 
 
-    // TODO: Fill in the space below with helper methods
-
-    /**
-     * greatestCommonDivisor - Find the largest integer that evenly divides two integers.
-     *      Use this helper method in the Final Checkpoint to reduce fractions.
-     * @param a - First integer.
-     * @param b - Second integer.
-     * @return The GCD.
-     */
-/*
-    public static int greatestCommonDivisor(int a, int b){
-
-      return 0;
+    public static String simpification(String term){
+      int x1 = numerator2(term);
+      int y1 = denominator2(term);
+      int gcd = 1;
+      for(int i = 1; i <= x1 && i <= y1; ++i){
+        if(x1%i==0 && y1%i==0)
+          gcd = i; }
+      String result = (x1 / gcd) + "/" + (y1 / gcd);
+      return result;
     }//end greatestCommonDivisor method
 
-    /**
-     * leastCommonMultiple - Find the smallest integer that can be evenly divided by two integers.
-     *      Use this helper method in Checkpoint 3 to evaluate expressions.
-     * @param a - First integer.
-     * @param b - Second integer.
-     * @return The LCM.
-     */
-/*
-    public static int leastCommonMultiple(int a, int b){
-
-      return 0;
-    }//end leastCommonMultiple
-*/
 }//end class
