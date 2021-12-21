3f3e20fddd768ed30841a29cd506c160e2a557b1
Qijin Huang <qijinh@nycstudents.net>
2021-12-21 08:45:37 -0500

    12/21/21

--- 08-Binary/Binary.java
+++ 08-Binary/Binary.java
@@ -5,10 +5,12 @@ public class Binary {
   public static void main(String[] arg){
     Scanner s = new Scanner(System.in);
     System.out.println("What is your binary number?");
-    String Num = s.nextLine();
-
-    
+    int Num = s.nextInt();
+    System.out.println("Your binary number is: " + binary(Num));
   }
 
+  public static int binary(int num){
+
+  }
 
 }
