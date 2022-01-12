300cddd49ea4f613b9b0470f3b4344b48f0709ab
Qijin Huang <qijinh@nycstudents.net>
2022-01-12 08:30:04 -0500

    01/12/2022

--- 11-arraysOfArray/GradeList.java
+++ 11-arraysOfArray/GradeList.java
@@ -4,13 +4,17 @@ public class GradeList {
 
   public static void main (String[] args) {
 
-    Scanner s = new Scanner(System.in);
     int[][] gradeList = new int[15][5];
 
-    for (int[] grades : gradeList){
-      for (int grade : grades){
-        grade = (int)(Math.random() * 46) + 55;
-        System.out.print(grade + " ");
+    for (int i = 0; i < gradeList.length; i++) {
+      for (int j = 0 ; j < gradeList[i].length; j++){
+        gradeList[i][j] = (int)(Math.random() * 46) + 55;
+      }
+    }
+
+    for(int[] grades : gradeList) {
+      for(int grade : grades) {
+          System.out.print(grade + " ");
       }
       System.out.println();
     }
